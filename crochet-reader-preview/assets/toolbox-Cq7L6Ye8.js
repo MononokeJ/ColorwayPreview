9bda201383fa138b98b6c506eb57704143dcc93d
MononokeJ <619308482@qq.com>
2026-09-11 15:59:43 +0800

    Update CrochetReader preview and Hello Sanshi library

--- crochet-reader-preview/assets/toolbox-Cq7L6Ye8.js
+++
@@ -1,1 +0,0 @@
-import{m as e}from"./acceptance-badge-DodOg5mt.js";import{i as o}from"./i18n-qVZyI_jy.js";o.applyDocument();const t=document.querySelector("#language-switch");t&&o.mountLocaleSwitch(t);e(o);
